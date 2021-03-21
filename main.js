f78b12d72d2d382ab09d23265c4871acb879dd44
Benni <benni@posteo.de>
2021-03-21 18:07:45 +0100

    Save message in webstore.

--- main.js
+++ main.js
@@ -8,14 +8,21 @@ const app = Vue.createApp({
       ]
     }
   },
+  mounted() {
+    if (localStorage.messages) {
+      this.messages = JSON.parse(localStorage.messages);
+    }
+  },
   methods: {
     addMessage() {
       this.messages.push(this.inputMessage)
+      localStorage.messages = JSON.stringify(this.messages)
 
       this.inputMessage = ""
     },
     deleteMessage(id) {
       this.messages.splice(id, 1)
+      localStorage.messages = JSON.stringify(this.messages)
       return
     }
   }
